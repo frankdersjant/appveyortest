2ce92276222aa3ba930eda31fca6c5ba6b1277b2
frankdersjant <dersjantf@gmail.com>
2018-03-11 12:44:22 +0100

    test3

---
+++ Appveyortest/Controllers/NugetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Appveyortest.Controllers
+{
+    public class NugetController : Controller
+    {
+        // GET: Nuget
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
